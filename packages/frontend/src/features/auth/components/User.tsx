e0c35f21b56c0b444634044fa91c6ce0b8225989
fujitashuichi <fujitashuu14@gmail.com>
2026-03-18 22:14:12 +0900

    refactor(FE): UI設計最適化

---
+++ packages/frontend/src/features/auth/components/User.tsx
@@ -0,0 +1,41 @@
+import { AppButton } from '../../../components';
+import { AppLoadingBar } from '../../../components/AppLoadingBar';
+import { useAuth } from '../../../Context';
+
+export function User() {
+  const { session, useUser, getUser } = useAuth();
+
+  const { status: sessionStatus } = session;
+  const { user } = useUser;
+  const { status: gettingStatus, errorMessage } = getUser;
+
+
+  if (sessionStatus === "idle") return <h1>ログインしていください</h1>
+
+  if (user === null) return <h1>データがありません</h1>;
+
+  return (<>
+    {gettingStatus === "idle" || gettingStatus === "loading" &&
+      <AppLoadingBar className="fixed top-0 left-1/2 -translate-x-1/2 z-10 w-20 h-1.5" />
+    }
+
+    {gettingStatus === "failed" &&
+      <>
+        <h1>{errorMessage}</h1>
+        <AppButton
+          variant="primary"
+          onClick={() => window.location.reload()}
+        >再試行</AppButton>
+      </>
+    }
+
+    {gettingStatus === "success" &&
+      <div>
+        <h2>email</h2>
+        <p>{user.email}</p>
+        <h2>作成日時</h2>
+        <p>{new Date(user.createdAt).toLocaleString()}</p>
+      </div>
+    }
+  </>)
+}
--- packages/frontend/src/features/auth/components/index.ts
+++ packages/frontend/src/features/auth/components/index.ts
@@ -1,3 +1,4 @@
 export { RegisterForm } from "./RegisterForm.tsx"
 export { LoginContainer } from "./LoginContainer.tsx"
 export { LogoutButton } from "./LogoutButton.tsx"
+export { User } from "./User.tsx"
