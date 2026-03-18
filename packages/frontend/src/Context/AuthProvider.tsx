d68e56e94b6721969c5ed9ecf7a74d77a7a11222
fujitashuichi <fujitashuu14@gmail.com>
2026-03-18 20:30:46 +0900

    feature(FE: AuthProvider): ログイン時のデータ取得を実装

--- packages/frontend/src/Context/AuthProvider.tsx
+++ packages/frontend/src/Context/AuthProvider.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo } from 'react'
 import { AuthCtx, type AuthCtxType } from './AuthContext'
 import { useLogin, useLogout, useRegister, useSessionStatus, useUser } from '../features/auth/hooks';
 import { isSessionActive } from '../features/auth/api';
+import { useGetUserData } from '../features/auth/hooks/useGetUserData';
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const useSessionHook = useSessionStatus();
@@ -9,25 +10,33 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const useLoginHook = useLogin(useSessionHook);
   const useLogoutHook = useLogout(useSessionHook);
   const userData = useUser();
+  const useGetUser = useGetUserData(userData.setUser);
 
   useEffect(() => {
-    const check = async () => {
+    // セッションを10分おきにチェック
+    const checkSession = async () => {
       const isLoggedIn = await isSessionActive();
-      if (isLoggedIn) useSessionHook.setStatus("active");
+      useSessionHook.setStatus(isLoggedIn ? "active" : "idle");
     };
-    check();
+    checkSession();
 
-    const interval = setInterval(async () => await check(), 10 * 60 * 1000);
+    const interval = setInterval(async () => await checkSession(), 10 * 60 * 1000);
     return () => clearInterval(interval);
-  }, []);
+  }, [useSessionHook]);
+
+  useEffect(() => {
+    if (useSessionHook.status === "active") useGetUser.getUser();
+  }, [useSessionHook.status, useGetUser]);
+
 
   const ctxData: AuthCtxType = useMemo(() => ({
     session: useSessionHook,
     register: useRegisterHook,
     login: useLoginHook,
     logout: useLogoutHook,
-    user: userData
-  }), [useSessionHook, useRegisterHook, useLoginHook, useLogoutHook, userData]);
+    user: userData,
+    getUser: useGetUser
+  }), [useSessionHook, useRegisterHook, useLoginHook, useLogoutHook, userData, useGetUser]);
 
   return (
     <AuthCtx.Provider value={ctxData}>
