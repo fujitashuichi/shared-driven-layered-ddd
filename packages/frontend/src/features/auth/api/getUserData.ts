adf65ed85d795ea09864111fc6123eccfe3f2acf
fujitashuichi <fujitashuu14@gmail.com>
2026-03-18 19:02:06 +0900

    feature(FE: api): getUserDataを追加

---
+++ packages/frontend/src/features/auth/api/getUserData.ts
@@ -0,0 +1,38 @@
+import { MeResponseSchema } from "@pkg/shared";
+import { apiClient } from "../../../lib"
+import type { GetUserDataResult } from "../types";
+
+export const getUserData = async (): Promise<GetUserDataResult> => {
+  const response = await apiClient({
+    path: "/api/auth/me",
+    method: "POST",
+    body: undefined
+  });
+
+  if (!response.ok) {
+    if (response.status === 401) {
+      return {
+        ok: false,
+        errorType: "UnAuthorized"
+      }
+    }
+
+    return {
+      ok: false,
+      errorType: "UnknownError"
+    }
+  }
+
+  const parsed = MeResponseSchema.safeParse(response.body);
+  if (!parsed.success) {
+    return {
+      ok: false,
+      errorType: "InvalidData"
+    }
+  }
+
+  return {
+    ok: true,
+    data: parsed.data
+  }
+}
