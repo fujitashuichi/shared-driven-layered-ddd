7d903a80bab3f5181c83266ea9d2d1510e9f3e16
fujitashuichi <fujitashuu14@gmail.com>
2026-03-21 21:41:37 +0900

    feature(BE): testスクリプトを完成

--- packages/backend/src/scripts/runTest.ts
+++ packages/backend/src/scripts/runTest.ts
@@ -1,13 +1,21 @@
 import * as dotenv from "dotenv"
 import { spawnSync } from "node:child_process";
+import path from "node:path";
+import { styleText } from "node:util";
 
-dotenv.config({ path: "../../.env.test" });
+
+const envPath = path.join(import.meta.dirname, "../../.env.test");
+dotenv.config({ path: envPath });
+
 
 const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) throw new Error("Could not start test because DATABASE_URL was undefined");
+else process.stdout.write(styleText(["green"], "ENV>> DATABASE_URL successfully loaded"));
 
 
-spawnSync('tsx', ['vitest', ...process.argv.slice(2)], {
+const npxCommand = process.platform === "win32" ? "npx.cmd" : "npx";
+
+spawnSync(npxCommand, ['vitest', ...process.argv.slice(2)], {
   stdio: "inherit",
   shell: true,
   env: { ...process.env }
