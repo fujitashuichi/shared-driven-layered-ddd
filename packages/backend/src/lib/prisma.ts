4c2f545caed7c5c8429fe65f82abf0d039c374e2
fujitashuichi <fujitashuu14@gmail.com>
2026-03-24 11:59:19 +0900

    fix(BE): prismaのアウトプットディレクトリを修正

--- packages/backend/src/lib/prisma.ts
+++ packages/backend/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { ENV } from "../config/env.js";
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client/extension";
 
 export const prisma = new PrismaClient({
   adapter: new PrismaPg({ connectionString: ENV.DATABASE_URL })
