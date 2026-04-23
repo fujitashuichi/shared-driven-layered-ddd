d6d341b1d7bc53746f9695ab0301a27ee82f47ae
fujitashuichi <fujitashuu14@gmail.com>
2026-04-24 01:06:05 +0900

    style: 可読性向上

--- packages/frontend/src/Context/AuthContext.ts
+++ packages/frontend/src/Context/AuthContext.ts
@@ -2,35 +2,47 @@ import type { User } from "@pkg/shared";
 import type { MutationStatus } from "@tanstack/react-query";
 import React, { createContext, useContext, type SetStateAction } from "react";
 
+
+/* 各Hookの型定義 ↓ */
+
 type Register = {
   status: MutationStatus,
   register: (e: React.SubmitEvent<HTMLFormElement>) => void
 };
+
 type Login = {
   status: Exclude<MutationStatus, "success"> | "loggedIn",
   errorMessage: string | null
   login: (e: React.SubmitEvent<HTMLFormElement>) => void
 };
+
 type Logout = {
   status: Exclude<MutationStatus, "success"> | "loggedOut",
   logout: () => void
 };
+
 type Session = {
   status: "idle" | "inactive" | "active",
   setStatus: React.Dispatch<Session["status"]>
 };
+
 type GetSession = {
   getSession: () => Promise<void>
 };
+
 type GetUser = {
   status: MutationStatus,
   errorMessage: string | null,
   getUser: () => Promise<void>
 };
+
 type UseUser = {
   user: User | null,
   setUser: React.Dispatch<SetStateAction<User | null>>
 };
+
+/* ↑ 各Hookの型定義 */
+
 
 export type AuthCtxType = {
   register: Register, login: Login, logout: Logout, session: Session, getSession: GetSession, getUser: GetUser, useUser: UseUser
--- packages/frontend/src/Context/ProjectContext.ts
+++ packages/frontend/src/Context/ProjectContext.ts
@@ -2,33 +2,43 @@ import type { Project } from "@pkg/shared";
 import type { MutationStatus } from "@tanstack/react-query";
 import React, { createContext, useContext, type SetStateAction } from "react";
 
+
+/* 各Hookの型定義 ↓ */
+
 type ProjectsData = {
   projects: Project[],
   setProjects: React.Dispatch<SetStateAction<Project[]>>
 }
+
 type GetProjects = {
   status: MutationStatus,
   errorMessage: string | null,
   get: () => Promise<void>
 }
+
 type Create = {
   status: MutationStatus,
   errorMessage: string | null,
   create: (e: React.SubmitEvent<HTMLFormElement>) => Promise<void>,
   reset: () => void
 };
+
 type Update = {
   status: MutationStatus,
   errorMessage: string | null,
   update: (e: React.SubmitEvent<HTMLFormElement>, id: Project["id"]) => Promise<void>
   reset: () => void
 };
+
 type Delete = {
   status: MutationStatus,
   errorMessage: string | null,
   delete: (id: Project["id"]) => Promise<void>
   reset: () => void
 };
+
+/* ↑各Hookの型定義 */
+
 
 export type ProjectCtxType = {
   projectsData: ProjectsData, getProjects: GetProjects, create: Create, update: Update, delete: Delete
